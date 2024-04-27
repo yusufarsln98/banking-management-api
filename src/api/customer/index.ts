92e9549c977eddef5b9869a79e80a27c1b487c02
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-28 00:13:09 +0300

    feat: error handling and relational deletetion

--- src/api/customer/index.ts
+++ src/api/customer/index.ts
@@ -1,42 +1,82 @@
 import express from 'express';
 import Customer, { ICustomer } from '../../models/customer.model';
 import { DeleteResult } from 'mongodb';
+import ErrorResponse from '../../interfaces/ErrorResponse';
+import Account from '../../models/account.model';
 
 const router = express.Router();
 
-router.get<{}, ICustomer[]>('/', async (req, res) => {
-  const customers = await Customer.find();
-  res.json(customers);
+router.get<{}, ICustomer[] | ErrorResponse | null>('/', async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.json(customers);
+  } catch (error: ErrorResponse | any) {
+    res.status(404).json({ message: error.message, stack: error.stack });
+  }
 });
 
-router.get<{ id: string }, ICustomer | null>('/:id', async (req, res) => {
-  const customer = await Customer.findById(req.params.id);
-  res.json(customer);
-});
-
-router.post<{}, ICustomer, ICustomer>('/', async (req, res) => {
-  const customer = await Customer.create(req.body);
-  res.json(customer);
-});
-
-router.put<{ id: string }, ICustomer | null, ICustomer>(
+router.get<{ id: string }, ICustomer | ErrorResponse | null>(
   '/:id',
   async (req, res) => {
-    const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.json(customer);
+    try {
+      const customer = await Customer.findById(req.params.id);
+      res.json(customer);
+    } catch (error: ErrorResponse | any) {
+      res.status(404).json({ message: error.message, stack: error.stack });
+    }
   },
 );
 
-router.delete<{}, DeleteResult>('/all', async (req, res) => {
-  const customers = await Customer.deleteMany({});
-  res.json(customers);
+router.post<{}, ICustomer | ErrorResponse | null>('/', async (req, res) => {
+  try {
+    const customer = await Customer.create(req.body);
+    res.json(customer);
+  } catch (error: ErrorResponse | any) {
+    res.status(400).json({ message: error.message, stack: error.stack });
+  }
 });
 
-router.delete<{ id: string }, ICustomer | null>('/:id', async (req, res) => {
-  const customer = await Customer.findByIdAndDelete(req.params.id);
-  res.json(customer);
-});
+router.put<{ id: string }, ICustomer | ErrorResponse | null>(
+  '/:id',
+  async (req, res) => {
+    try {
+      const customer = await Customer.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+      );
+      res.json(customer);
+    } catch (error: ErrorResponse | any) {
+      res.status(400).json({ message: error.message, stack: error.stack });
+    }
+  },
+);
+
+router.delete<{}, DeleteResult | ErrorResponse | null>(
+  '/all',
+  async (req, res) => {
+    try {
+      const customers = await Customer.deleteMany({});
+      res.json(customers);
+    } catch (error: ErrorResponse | any) {
+      res.status(404).json({ message: error.message, stack: error.stack });
+    }
+  },
+);
+
+router.delete<{ id: string }, ICustomer | ErrorResponse | null>(
+  '/:id',
+  async (req, res) => {
+    try {
+      const customer = await Customer.findByIdAndDelete(req.params.id);
+      if (customer) {
+        await Account.deleteMany({ customerId: customer._id });
+      }
+      res.json(customer);
+    } catch (error: ErrorResponse | any) {
+      res.status(404).json({ message: error.message, stack: error.stack });
+    }
+  },
+);
 
 export default router;
