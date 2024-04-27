26ca7e2d7bf5597405c6a462bad600d521729194
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-28 00:20:55 +0300

    feat: error handling

--- src/api/account/index.ts
+++ src/api/account/index.ts
@@ -2,55 +2,95 @@
 import express from 'express';
 import Account, { IAccount } from '../../models/account.model';
 import { DeleteResult } from 'mongodb';
+import ErrorResponse from '../../interfaces/ErrorResponse';
 
 const router = express.Router();
 
-router.get<{}, IAccount[]>('/', async (req, res) => {
-  const accounts = await Account.find();
-  res.json(accounts);
+router.get<{}, IAccount[] | ErrorResponse | null>('/', async (req, res) => {
+  try {
+    const accounts = await Account.find();
+    res.json(accounts);
+  } catch (error: ErrorResponse | any) {
+    res.status(404).json({ message: error.message, stack: error.stack });
+  }
 });
 
-router.get<{ id: string }, IAccount | null>('/:id', async (req, res) => {
-  const account = await Account.findById(req.params.id);
-  res.json(account);
-});
+router.get<{ id: string }, IAccount | ErrorResponse | null>(
+  '/:id',
+  async (req, res) => {
+    try {
+      const account = await Account.findById(req.params.id);
+      res.json(account);
+    } catch (error: ErrorResponse | any) {
+      res.status(404).json({ message: error.message, stack: error.stack });
+    }
+  },
+);
 
 // Find the total balance of a customer across all accounts.
-router.get<{ customerId: string }, number>(
-  '/totalBalance/:customerId',
-  async (req, res) => {
-    const accounts = await Account.find({ customerId: req.params.customerId });
+router.get<
+  { customerId: string },
+  { totalBalance: number } | ErrorResponse | null
+>('/totalBalance/:customerId', async (req, res) => {
+  try {
+    const accounts = await Account.find({
+      customerId: req.params.customerId,
+    });
     const totalBalance = accounts.reduce(
       (acc, account) => acc + account.balance,
       0,
     );
-    res.json(totalBalance);
-  },
-);
-
-router.post<{}, IAccount, IAccount>('/', async (req, res) => {
-  const account = await Account.create(req.body);
-  res.json(account);
+    res.json({ totalBalance });
+  } catch (error: ErrorResponse | any) {
+    res.status(404).json({ message: error.message, stack: error.stack });
+  }
 });
 
-router.put<{ id: string }, IAccount | null, IAccount>(
+router.post<{}, IAccount | ErrorResponse | null>('/', async (req, res) => {
+  try {
+    const account = await Account.create(req.body);
+    res.json(account);
+  } catch (error: ErrorResponse | any) {
+    res.status(400).json({ message: error.message, stack: error.stack });
+  }
+});
+
+router.put<{ id: string }, IAccount | ErrorResponse | null>(
   '/:id',
   async (req, res) => {
-    const account = await Account.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.json(account);
+    try {
+      const account = await Account.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      res.json(account);
+    } catch (error: ErrorResponse | any) {
+      res.status(400).json({ message: error.message, stack: error.stack });
+    }
   },
 );
 
-router.delete<{}, DeleteResult>('/all', async (req, res) => {
-  const accounts = await Account.deleteMany({});
-  res.json(accounts);
-});
+router.delete<{}, DeleteResult | ErrorResponse | null>(
+  '/all',
+  async (req, res) => {
+    try {
+      const accounts = await Account.deleteMany({});
+      res.json(accounts);
+    } catch (error: ErrorResponse | any) {
+      res.status(404).json({ message: error.message, stack: error.stack });
+    }
+  },
+);
 
-router.delete<{ id: string }, IAccount | null>('/:id', async (req, res) => {
-  const account = await Account.findByIdAndDelete(req.params.id);
-  res.json(account);
-});
+router.delete<{ id: string }, IAccount | ErrorResponse | null>(
+  '/:id',
+  async (req, res) => {
+    try {
+      const account = await Account.findByIdAndDelete(req.params.id);
+      res.json(account);
+    } catch (error: ErrorResponse | any) {
+      res.status(404).json({ message: error.message, stack: error.stack });
+    }
+  },
+);
 
 export default router;
